a3ff66e264eff487498a451c06fe7cef0674eae3
supharatnop <33540580+supharatnop@users.noreply.github.com>
2020-03-06 12:45:21 +0700

    UIQuiz

---
+++ app/src/main/java/com/cis/lab/uiquiz/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.cis.lab.uiquiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button2.setOnClickListener {
+            var i = Intent(this,Main2Activity::class.java)
+            startActivity(i)
+        }
+
+    }
+}
